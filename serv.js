fdd89c637a817a8bed7aa24cc8f15181472141bc
Vlad50-50 <auditorrevizor@gmail.com>
2024-05-06 18:04:22 +0300

    06.05.24

--- serv.js
+++ serv.js
@@ -18,12 +18,10 @@ app.get('/', (req, res) => {
     else{
         res.status(200).send('ok');
     }
-
 });
 server.listen(3000, () => {
     console.log("Погнали!");
 })
-
 
 io.on('connection', (socket) => {
     console.log("New user: " + socket.id);
@@ -33,35 +31,26 @@ io.on('connection', (socket) => {
         const base64Image = data.image;
         const imageBuffer = Buffer.from(base64Image, 'base64');
         fs.writeFile(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'), imageBuffer, err => {
-            // if (err) {
-            //     console.error(err);
-            // } else {
-                
-                console.log("it will be uploaded");
-                // const auth = new google.auth.GoogleAuth({
-                //     keyFile: path.join(__dirname,"security.json"),
-                //     scopes: ['https://www.googleapis.com/auth/drive'],
-                // });
-                // const drive = google.drive({version: 'v3', auth});
-                // const response = drive.files.create({
-                //     requestBody: {
-                //         name: headers["name"]+getCurrentDate(),
-                //         mimeType: 'image/jpg',
-                //         parents: ['1HskEq4Ss7iKtpsuh8h9mp2zr1LqHALTw']
-                //     },
-                //     media: {
-                //         mimeType: 'image/jpg',
-                //         body: fs.createReadStream(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'))
-                //     }
-                // });
-                socket.emit('image_processed')
-            // }
+            console.log("it will be uploaded");
+            // const auth = new google.auth.GoogleAuth({
+            //     keyFile: path.join(__dirname,"security.json"),
+            //     scopes: ['https://www.googleapis.com/auth/drive'],
+            // });
+            // const drive = google.drive({version: 'v3', auth});
+            // const response = drive.files.create({
+            //     requestBody: {
+            //         name: headers["name"]+getCurrentDate(),
+            //         mimeType: 'image/jpg',
+            //         parents: ['1HskEq4Ss7iKtpsuh8h9mp2zr1LqHALTw']
+            //     },
+            //     media: {
+            //         mimeType: 'image/jpg',
+            //         body: fs.createReadStream(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'))
+            //     }
+            // });
+            socket.emit('image_processed')
         });
     });
-    socket.on('message', (data) => {
-        console.log('Сообщение от клиента: ', data);
-      });
-
     socket.on('disconnect', () => {
         console.log("User is disconected: " + socket.id);
     });
